fa2ef92e959d22e2b1762eb8f45f36d235cf1557
arafat-rahman01 <aabrarbin851@gmail.com>
2025-09-18 01:10:32 +0600

    Accepted

---
+++ BigCountery.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE WorldDB;
+USE WorldDB;
+
+CREATE TABLE World (
+    name VARCHAR(50) PRIMARY KEY,
+    continent VARCHAR(20),
+    area BIGINT,
+    population BIGINT,
+    gdp BIGINT
+);
+
+INSERT INTO World (name, continent, area, population, gdp) VALUES
+('Afghanistan', 'Asia', 652230, 25500100, 20343000000),
+('Albania', 'Europe', 28748, 2831741, 12960000000),
+('Algeria', 'Africa', 2381741, 37100000, 188681000000),
+('Andorra', 'Europe', 468, 78115, 3712000000),
+('Angola', 'Africa', 1246700, 20609294, 100990000000);
+
+SELECT name,population,area
+FROM World
+where area>=3000000 or population>=25000000;
